080991e87a2ee898ff7dfabc9c6e9c1f55c906bf
SongSongSun <49385128+songsongsun@users.noreply.github.com>
2020-07-07 17:44:51 +0800

    简易框架搭建

---
+++ src/main/java/com/song/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.song.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author Song
+ * @Date 2020/7/7 16:29
+ * @Version 1.0
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = "我的简易框架";
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("WEB-INF/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,11 @@
+<%@page pageEncoding="UTF-8" %>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<h1>太牛逼了，${name}</h1>
+</body>
+</html>
